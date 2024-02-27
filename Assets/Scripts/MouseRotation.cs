478499846ac0499e46bbb646efa4e302e9cb77f8
ferry <ulyanovskiy.2001@mail.ru>
2024-02-27 20:35:36 +0300

    Improve mouse rotation

--- Assets/Scripts/MouseRotation.cs
+++ Assets/Scripts/MouseRotation.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public sealed class MouseRotation : MonoBehaviour
 {
-    public Vector2 turn;
+    [Range(1f, 10f)] public float sensitivity = 5f;
+    [Range(0f, 90f)] public float yRotationLimit = 90f;
+
+    private Vector2 turn;
 
     private void Update()
     {
         turn.x += Input.GetAxis("Mouse X");
         turn.y += Input.GetAxis("Mouse Y");
+
+        turn.y = Mathf.Clamp(turn.y, -yRotationLimit, yRotationLimit);
 
         transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
     }
